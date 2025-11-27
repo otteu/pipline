20ac7ccafe004058cc8b6509138e552538f316af
otteu <otteu1m@gmail.com>
2025-11-27 16:47:04 +0900

    import func

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -18,7 +18,7 @@ from common.common_fun import get_sftp
 with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2025, 11, 27 tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     task_get_sftp = PythonOperator(
